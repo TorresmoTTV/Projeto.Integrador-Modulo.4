2727044564b73cefb50aee9e1922e2cc0f9c8fdd
TorresmoTTV <luis-lembi@hotmail.com>
2025-04-16 18:45:54 -0300

    Update 16.04.25 - 2

--- view/editar-cliente.php
+++ view/editar-cliente.php
@@ -3,7 +3,7 @@ session_start();
 require '../DAO/conexao.php';
 
 if (!isset($_SESSION['user_id'])) {
-    header('Location: ../index.html');
+    header('Location: ../index.php');
     exit();
 }
 
